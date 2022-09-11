a7812bd19a1cd2d63129fa50bbfc8c10069f39b9
geekplux <geekplux@gmail.com>
2022-09-11 22:01:40 +0800

    chore: inject analytics script

---
+++ inject_analytics.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sed -i '' 's/<\/head>/<script async defer data-website-id="cac6cb9d-c349-486c-bc93-46cb395c8005" src="https:\/\/geekplux.up.railway.app\/umami.js"><\/script><\/head>/' public/index.html
